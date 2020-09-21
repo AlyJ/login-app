c2e70e8fb3c111d73c51db8713d250cd85b7a44c
AlyJ <alina.jarmolica2@gmail.com>
2020-09-21 16:26:49 +0300

    Initial Commit

---
+++ login app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  login app
+//
+//  Created by alina.jarmolica on 21/09/2020.
+//  Copyright © 2020 alina.jarmolica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
